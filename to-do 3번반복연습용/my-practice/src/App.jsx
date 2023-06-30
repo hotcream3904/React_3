266c3c9aefc596a40750365dcda01c45f341636f
hotcream3904 <dkssud6757@naver.com>
2023-06-30 11:19:15 +0900

    GlobalStyles

--- to-do 3번반복연습용/my-practice/src/App.jsx
+++ to-do 3번반복연습용/my-practice/src/App.jsx
@@ -1,41 +1,14 @@
-import styled from 'styled-components';
+import TestPage from './components/TestPage';
+import GlobalStyle from './components/GlobalStyle';
 
 function App() {
   return (
-    <StContainer>
-      {
-      BoxList.map((boxColor)=>{
-        return <StBox borderColor={boxColor}>{getBoxName(boxColor)}</StBox>
-      })
-      }
-    </StContainer>
+    <>
+    <GlobalStyle/>
+    <TestPage title="제목입니다" contents="내용입니다"/>
+    </>
   );
 }
 
-const BoxList = ['red', 'blue', 'green', 'b'];
-
-const getBoxName = (color) => {
-  switch (color) {
-    case 'red':
-      return '빨간 박스';
-    case 'green':
-      return '초록 박스';
-    case 'blue':
-      return '파랑 박스';
-    default:
-      return '검정 박스';
-  }
-};
-
-const StContainer = styled.div`
-  display: flex;
-`;
-
-const StBox = styled.div`
-  width: 100px;
-  height: 100px;
-  border: 1px solid ${(props) => props.borderColor}; //얘네가 지금 상속받고있음.
-  margin: 20px;
-`;
 
 export default App;
